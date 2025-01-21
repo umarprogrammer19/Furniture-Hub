d0bcf1097a68b024ec47b3931c814cbdabb24e1e
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-01-22 03:28:43 +0500

    Use Context

---
+++ context/searchContext.js
@@ -0,0 +1,22 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const SearchContext = createContext(undefined);
+
+export const SearchProvider = ({ children }) => {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    return (
+        <SearchContext.Provider value={{ searchQuery, setSearchQuery }}>
+            {children}
+        </SearchContext.Provider>
+    );
+};
+
+export const useSearch = () => {
+    const context = useContext(SearchContext);
+    if (!context) {
+        throw new Error("useSearch must be used within a SearchProvider");
+    }
+    return context;
+};
